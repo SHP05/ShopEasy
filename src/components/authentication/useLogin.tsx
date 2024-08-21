04edef42fc5732b58cd797cfe7d5abc69a17107b
Suman Pankhaniya <117751876+SHP05@users.noreply.github.com>
2024-08-21 15:53:49 +0530

    Implement Protected Routes

--- src/components/authentication/useLogin.tsx
+++ src/components/authentication/useLogin.tsx
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuth';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export function useLogin() {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const { mutate: login, isPending } = useMutation({
     mutationFn: (data: loginType) => {
@@ -14,6 +16,8 @@ export function useLogin() {
       console.log(user);
       queryClient.setQueryData(['user'], user);
       toast.success('Login Successfully');
+      window.localStorage.setItem('loggedIn', 'true');
+      navigate('/home');
     },
     onError: (err) => {
       console.log('QueryErr: ', err);
