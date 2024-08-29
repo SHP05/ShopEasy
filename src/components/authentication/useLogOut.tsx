ff067ab96f20b7b3aedd8601176c20fb24e0b923
Shyam Pankhaniya <shyam.j.pankhaniya555@gmail.com>
2024-08-29 09:39:23 +0530

    Update Header and protetcted Routes

--- src/components/authentication/useLogOut.tsx
+++ src/components/authentication/useLogOut.tsx
@@ -1,24 +1,23 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import toast from 'react-hot-toast';
 import { logout as logoutApi } from '../../services/apiAuth';
-import { useNavigate } from 'react-router-dom';
 
 export function useLogOut() {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
+  console.log();
 
-  const { mutate: logout, isPending } = useMutation({
+  const {
+    mutate: logout,
+    isPending,
+    isSuccess,
+  } = useMutation({
     mutationFn: () => logoutApi(),
     onSuccess: () => {
       queryClient.clear();
-      toast.success('Logout Successfully');
-      window.localStorage.setItem('loggedIn', 'false');
-      navigate('/');
     },
     onError: (err) => {
       console.log('QueryError: ', err);
     },
   });
 
-  return { logout, isPending };
+  return { logout, isPending, isSuccess };
 }
